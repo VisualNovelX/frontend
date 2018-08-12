d10f5b0df6f44684cad1573b015a064bd30651a3
Maximiliano Escobar <maximiliano.escobar@rga.com>
2018-08-12 13:40:39 -0300

    Update routes

--- src/views/views.routing.ts
+++ src/views/views.routing.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeView } from 'views/home/home.view';
+import { ShopView } from 'views/shop/shop.view';
+import { BlogView } from 'views/blog/blog.view';
 
 
 const ViewsRoutes: Routes = [
     { path: '', component: HomeView },
+    { path: 'blog', component: BlogView },
+    { path: 'shop', component: ShopView },
 ];
   
 export const ViewsRouting = RouterModule.forRoot(
