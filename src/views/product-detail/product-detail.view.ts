af80db5ea6ebf6a7f6a37eb3597b93344e850443
Maximiliano Escobar <maximiliano.escobar@rga.com>
2018-08-12 13:39:23 -0300

    Add ProductDetailView

---
+++ src/views/product-detail/product-detail.view.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-detail-view',
+  templateUrl: './product-detail.view.html',
+  styleUrls: ['./product-detail.view.scss']
+})
+export class ProductDetailView {
+    
+}
