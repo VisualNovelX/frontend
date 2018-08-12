6920bbe6c59b62ab0c635fd79bd144e7f43004e5
Maximiliano Escobar <maximiliano.escobar@rga.com>
2018-08-12 13:42:12 -0300

    Update ViewsModule

--- src/views/views.module.ts
+++ src/views/views.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
+
 import { HomeView } from 'views/home/home.view';
+import { ProductDetailView } from 'views/product-detail/product-detail.view';
+import { ShopView } from 'views/shop/shop.view';
+
 import { ComponentsModule } from 'components/components.module';
+import { BlogView } from './blog/blog.view';
+
 
 @NgModule({
   declarations: [
-    HomeView
+    HomeView,
+    ShopView,
+    BlogView,
+    ProductDetailView
   ],
   imports: [
     ComponentsModule,
